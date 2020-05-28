b97994097d2ec2f202faef24ce665d93053437e0
jorgesoto98 <jorgesoto98@users.noreply.github.com>
2020-05-27 23:37:03 -0500

    Camara Follow Player I

---
+++ Plataformer Proyecto Videojuegos/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Vector3 posicion;
+    public GameObject Player;
+
+    void Start()
+    {
+        posicion = transform.position - Player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Player.transform.position + posicion;
+    }
+}
--- Plataformer Proyecto Videojuegos/Assets/Scripts/Parallax.cs
+++ Plataformer Proyecto Videojuegos/Assets/Scripts/Parallax.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     private float length, startpos;
-    public GameObject cam;
+    public GameObject Follow;
     public float parallaxEffect;
 
     void Start()
@@ -16,10 +16,10 @@ public class Parallax : MonoBehaviour
 
     void Update()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float temp = (Follow.transform.position.x * (1 - parallaxEffect));
+        float dist = (Follow.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startpos + dist, 0 , -1);
 
         if (temp > startpos + length) { startpos += length; }
         else if (temp < startpos - length) { startpos -= length; };
